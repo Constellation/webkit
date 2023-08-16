ef18c1c25afcd051a4004be8c8a949c45fee0b51
Yusuke Suzuki <ysuzuki@apple.com>
2023-08-16 16:53:07 -0700

    OK

---
+++ Source/JavaScriptCore/runtime/NativeCallee.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+namespace JSC {
+
+class LLIntOffsetsExtractor;
+
+class NativeCallee : public ThreadSafeRefCounted<NativeCallee> {
+    WTF_MAKE_FAST_ALLOCATED;
+public:
+    enum class Type : uint8_t {
+        JSIC,
+        JSToWasmIC,
+
+        WasmLLInt,
+        WasmIPInt,
+        WasmBBQ,
+        WasmBBQForOSREntry,
+        WasmOMG,
+        WasmOMGForOSREntry,
+        WasmJSEntrypoint,
+        WasmToJS,
+    };
+
+    IndexOrName indexOrName() const { return m_indexOrName; }
+    Type type() const { return m_type; }
+    ImplementationVisibility implementationVisibility() const { return m_implementationVisibility; }
+
+    CodePtr<WasmEntryPtrTag> entrypoint() const;
+    RegisterAtOffsetList* calleeSaveRegisters();
+    std::tuple<void*, void*> range() const;
+
+    const HandlerInfo* handlerForIndex(Instance&, unsigned, const Tag*);
+
+    bool hasExceptionHandlers() const { return !m_exceptionHandlers.isEmpty(); }
+
+    void dump(PrintStream&) const;
+
+    JS_EXPORT_PRIVATE void operator delete(NativeCallee*, std::destroying_delete_t);
+
+protected:
+    JS_EXPORT_PRIVATE NativeCallee(Type);
+    JS_EXPORT_PRIVATE NativeCallee(Type, size_t, std::pair<const Name*, RefPtr<NameSection>>&&);
+
+    template<typename Func>
+    void runWithDowncast(const Func&);
+    template<typename Func>
+    void runWithDowncast(const Func&) const;
+
+private:
+    const CompilationMode m_type;
+    ImplementationVisibility m_implementationVisibility { ImplementationVisibility::Public };
+    const IndexOrName m_indexOrName;
+
+protected:
+    FixedVector<HandlerInfo> m_exceptionHandlers;
+};
+
+} // namespace JSC
