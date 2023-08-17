38c4ae1617e09bac5687351e1f55a6a2392cf585
Yusuke Suzuki <ysuzuki@apple.com>
2023-08-16 17:10:59 -0700

    OK

---
+++ Source/JavaScriptCore/runtime/NativeCallee.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "NativeCallee.h"
+
+#include "LLIntExceptions.h"
+#include "WasmCalleeRegistry.h"
+#include "WasmCallingConvention.h"
+#include "WasmModuleInformation.h"
+
+namespace JSC {
+
+NativeCallee::NativeCallee(Type type)
+    : m_type(type)
+    , m_implementationVisibility(ImplementationVisibility::Private)
+{
+}
+
+NativeCallee::NativeCallee(Type type, size_t index, std::pair<const Name*, RefPtr<NameSection>>&& name)
+    : m_type(type)
+    , m_indexOrName(index, WTFMove(name))
+{
+}
+
+void NativeCallee::dump(PrintStream& out) const
+{
+    out.print(makeString(m_indexOrName));
+}
+
+void NativeCallee::operator delete(NativeCallee* callee, std::destroying_delete_t)
+{
+    CalleeRegistry::singleton().unregisterCallee(callee);
+    switch (m_type) {
+    case Type::Wasm:
+        Wasm::Callee::destroy(callee);
+        break;
+    case Type::InlineCache:
+        InlineCacheCallee::destroy(callee);
+        break;
+    }
+}
+
+} // namespace JSC
